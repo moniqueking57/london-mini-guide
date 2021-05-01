0542baaec6caf3c42bfc5ce58858e1d42ad0bf12
Monique <moniqueking57@gmail.com>
2021-05-01 14:47:09 +0100

    ..

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const cors = require('cors');
+
+const app = express();
+
+// app.use(cors());
+app.use(express.json());
+
+const messages = 'Hi';
+
+app.get('/', function (req, res) {
+  res.send(messages);
+});
+
+app.listen(process.env.PORT || 8081, () => {
+  console.log('Server is working');
+});
